8780471cfc453ad3ccc2294dfae4b2caeb368500
jk <13643827+AdHominem@users.noreply.github.com>
2026-04-15 23:58:08 +0200

    API integration (#6)
    * Update ci.yml
    
    * Untoggled test_bandit
    
    * API works now with authentication

--- tests/unit/test_agent_smith_stack.py
+++ tests/unit/test_agent_smith_stack.py
@@ -1,10 +1,10 @@
 import aws_cdk as core
 import aws_cdk.assertions as assertions
 
-from agent_smith.agent_smith_stack import AgentSmithStack
+from infra.agent_smith_stack import AgentSmithStack
 
 # example tests. To run these tests, uncomment this file along with the example
-# resource in agent_smith/agent_smith_stack.py
+# resource in infra/agent_smith_stack.py
 def test_sqs_queue_created():
     app = core.App()
     stack = AgentSmithStack(app, "agent-smith")
